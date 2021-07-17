3fc6c5404411cd6427c2b1db632c8d9d961dd1f5
joeldevel <christianjoel.developer@gmail.com>
2021-07-17 14:09:29 -0300

    Main modified

--- src/testing/git/TestingGit.java
+++ src/testing/git/TestingGit.java
@@ -1,17 +1,12 @@
-
 package testing.git;
 
-/**
- *
- * @author pepe
- */
 public class TestingGit {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("It seems it works");
     }
     
 }
