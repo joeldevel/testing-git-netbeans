88f1300557a8d73978b439051e0988e9dfb0b2b3
joeldevel <christianjoel.developer@gmail.com>
2021-07-17 14:01:01 -0300

    Testing git integration in netbeans project

---
+++ src/testing/git/TestingGit.java
@@ -0,0 +1,17 @@
+
+package testing.git;
+
+/**
+ *
+ * @author pepe
+ */
+public class TestingGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
